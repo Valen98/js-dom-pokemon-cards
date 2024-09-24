773a0bf7c862e40fd2e0daedeadc2cea6d2a95e6
Valen98 <leo.wahlandtcs@gmail.com>
2024-09-24 16:23:52 +0200

    You can now shift between different games and generation card

--- src/index.js
+++ src/index.js
@@ -21,8 +21,9 @@
 
 
 const cardUL = document.querySelector(".cards")
+const generationUL = document.querySelector('.generation')
 
-function renderCards() {
+function renderCards(generation) {
     cardUL.innerHTML = ""
     for(let i = 0; i < data.length; i++) {
         let pokemon = data[i]
@@ -38,14 +39,29 @@ function renderCards() {
         let name = document.createTextNode(pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1))
 
         h1.appendChild(name)
-
         let img = document.createElement('img')
-        img.src = data[i].sprites.other["official-artwork"].front_default
+        img.src = renderImage(pokemon, generation)
         img.className = 'card--img'
 
+        let buttonUL = document.createElement('ul')
+        let buttonLi = document.createElement('li')
+        buttonLi.className = 'buttonLi'
+        buttonUL.className = 'buttonUL'
+        let generationArray = ['official artwork','generation 1', 'generation 2', 'generation 3']
+        for(let i = 0; i < 4; i++) {
+          let button = document.createElement("button")
+          button.addEventListener("click", () => img.src = renderImage(pokemon, i))
+          button.textContent = generationArray[i] 
+          buttonLi.setAttribute('id', i)
+          buttonLi.className = 'generationButtons'
+          buttonLi.appendChild(button)
+        }
+
+        buttonUL.appendChild(buttonLi)
         pokemonLi.appendChild(h1)
-        console.log(h1)
         pokemonLi.appendChild(img)
+
+        pokemonLi.appendChild(buttonUL)
 
         for(let j = 0; j < pokemon.stats.length; j++) {
             let stats = pokemon.stats[j]
@@ -62,14 +78,37 @@ function renderCards() {
 
         }
 
+        for(let k = 0; k < pokemon.game_indices.length; k++) {
+          let game = pokemon.game_indices[k]
+          let pokemonGameLi = document.createElement('li')
+          pokemonGameLi.setAttribute('id', k)
+          pokemonGameLi.style = 'margin-left: 1rem; margin-bottom: 0.5rem;'
+          let p = document.createTextNode(game.version.name)
+
+          pokemonGameLi.appendChild(p)
+          pokemonLi.appendChild(pokemonGameLi)
+        }
 
         cardUL.appendChild(pokemonLi)
     }
 }
 
+function renderImage(pokemon, num) {
+  switch(num) {
+    case 1: 
+      return pokemon.sprites.versions['generation-i']['red-blue'].front_default
+    case 2:
+      return pokemon.sprites.versions['generation-ii'].crystal.front_default
+    case 3:
+      return pokemon.sprites.versions['generation-iii'].emerald.front_default
+    default:
+      return pokemon.sprites.other["official-artwork"].front_default
+  }
+}
+
 
 function main() {
-   renderCards()
+  renderCards(0)
 }
 
 main()
